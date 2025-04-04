8f20d2060f1dd57ef4e614ff263326e534af1d36
Jacob Fenwick <jacobfenwick111@gmail.com>
2025-04-03 18:48:09 -0700

    set up Go server to run

---
+++ apps/backend/go.mod
@@ -0,0 +1,3 @@
+module github.com/JakeFen/watchr/apps/backend
+
+go 1.24.2
--- apps/backend/main.go
+++ apps/backend/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello, world!")
+	})
+
+	fmt.Println("Starting server on :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println("Error starting server:", err)
+	}
 }
